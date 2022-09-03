a30594219e3b47d055951300b545e46bf10de823
LucasSampaioOliveira <lucassampaio33@hotmail.com>
2022-09-03 11:50:23 -0300

    first commit

---
+++ movies_lib/src/App.jsx
@@ -0,0 +1,18 @@
+import {Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+import './styles/main.scss';
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Navbar/>
+      <Outlet/>
+    </div>
+  )
+}
+
+export default App
